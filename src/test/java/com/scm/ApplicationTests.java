7c9290737040a09c21251de5d7f1e8b7d3f364c1
ASMITA GHOSH <138896766+gasmi26@users.noreply.github.com>
2025-05-01 17:38:58 +0530

    Update ApplicationTests.java

--- src/test/java/com/scm/ApplicationTests.java
+++ src/test/java/com/scm/ApplicationTests.java
@@ -16,32 +16,8 @@ class ApplicationTests {
 
 	void contextLoads() {
 	}
-//
-//	@Autowired
-//	private EmailService service;
-//
-//
-//	void sendEmailTest() {
-//		service.sendEmail("batchlcwd@gmail.com", "Just managing the emails",
-//				"this is scm project working on email service");
-//	}
-//
-//
-//	void testUnits() {
-//
-//
-//		int result=40;
-//
-//		 List<String>  list = List.of("ram","shyam","ankit");
-//
-////		assertThat(result).isEqualTo(50);
-//
-//		 assertThat(list).asList().size().isGreaterThan(5);
-//
-//
-//
-//
-//	}
+
+
 
 	@Autowired
 private EmailService service;
@@ -49,7 +25,7 @@ private EmailService service;
 	@Test
 
 		void sendEmailTest(){
-			service.sendEmail("asmitaghosh2004@gmail.com", "Just testing email service", "THIS IS JUST TESTING");
+			service.sendEmail(" ", "Just testing email service", "THIS IS JUST TESTING");
 		}
 
 
