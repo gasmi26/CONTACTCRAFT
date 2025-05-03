e656992eebb811d2449c93115ff997170bb7441d
ASMITA GHOSH <138896766+gasmi26@users.noreply.github.com>
2025-05-03 23:05:32 +0530

    Update UserRepo.java

--- src/main/java/com/scm/repsitories/UserRepo.java
+++ src/main/java/com/scm/repsitories/UserRepo.java
@@ -9,9 +9,7 @@ import com.scm.entities.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
-    // extra methods db relatedoperations
-    // custom query methods
-    // custom finder methods
+ 
 
     Optional<User> findByEmail(String email);
 
